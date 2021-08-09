44af25ea0c66ce0d4398d677c7fdb9804bc02c8d
HADi77 <hadi77@gmail.com>
2021-08-09 18:59:15 -0400

    Server-side Data

--- ReactDemo/Controllers/HomeController.cs
+++ ReactDemo/Controllers/HomeController.cs
@@ -3,14 +3,49 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ReactDemo.Models;
 
 namespace ReactDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            return Json(_comments);
         }
     }
 }
