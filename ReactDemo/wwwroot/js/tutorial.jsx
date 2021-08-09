235e5d84dd4351340fef406efafeec83f3bbea66
HADi77 <hadi77@gmail.com>
2021-08-09 18:47:24 -0400

    your first component

--- ReactDemo/wwwroot/js/tutorial.jsx
+++ ReactDemo/wwwroot/js/tutorial.jsx
@@ -1,1 +1,9 @@
-﻿
+﻿class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">Hello, world! I am a CommentBox.</div>
+        );
+    }
+}
+
+ReactDOM.render(<CommentBox />, document.getElementById('content'));
