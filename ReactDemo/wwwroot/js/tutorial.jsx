fa1a7e4a92fc234a2e39c3794fa93a1cd8bbcaa8
HADi77 <hadi77@gmail.com>
2021-08-09 18:48:38 -0400

    Composing components

--- ReactDemo/wwwroot/js/tutorial.jsx
+++ ReactDemo/wwwroot/js/tutorial.jsx
@@ -1,7 +1,27 @@
-﻿class CommentBox extends React.Component {
+﻿class CommentList extends React.Component {
     render() {
         return (
-            <div className="commentBox">Hello, world! I am a CommentBox.</div>
+            <div className="commentList">Hello, world! I am a CommentList.</div>
+        );
+    }
+}
+
+class CommentForm extends React.Component {
+    render() {
+        return (
+            <div className="commentForm">Hello, world! I am a CommentForm.</div>
+        );
+    }
+}
+
+class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
         );
     }
 }
