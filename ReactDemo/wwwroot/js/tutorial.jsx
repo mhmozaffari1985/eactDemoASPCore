3832a87ce897130ccaa5a10afa02111193f9b9cb
HADi77 <hadi77@gmail.com>
2021-08-09 18:50:21 -0400

    Using props

--- ReactDemo/wwwroot/js/tutorial.jsx
+++ ReactDemo/wwwroot/js/tutorial.jsx
@@ -1,7 +1,26 @@
-﻿class CommentList extends React.Component {
+﻿class Comment extends React.Component {
     render() {
         return (
-            <div className="commentList">Hello, world! I am a CommentList.</div>
+            <div className="comment">
+                <h2 className="commentAuthor">{this.props.author}</h2>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+class CommentList extends React.Component {
+    render() {
+        return (
+            <div className="commentList">
+                <Comment author="Daniel Lo Nigro">
+                    Hello ReactJS.NET World!
+                </Comment>
+                <Comment author="Pete Hunt">This is one comment</Comment>
+                <Comment author="Jordan Walke">
+                    This is *another* comment
+                </Comment>
+            </div>
         );
     }
 }
